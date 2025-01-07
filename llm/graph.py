932910b94598fef7c8d3f1dc86f647df6603a201
korzepadawid <korzepadawid@yahoo.com>
2025-01-07 18:07:11 +0100

    tmp

--- llm/graph.py
+++ llm/graph.py
@@ -172,6 +172,7 @@ def translate_pl_to_en(state: GraphState) -> dict:
     - Zwróć **tylko** przetłumaczone zdanie w języku angielskim.
 
     """
+    llm = BielikLLM(template)
     prompt = f"Tekst do przetłumaczenia: {state['question']}"
     answer = llm.predict(prompt)
     logger.info("Translated to English: %s", answer)
